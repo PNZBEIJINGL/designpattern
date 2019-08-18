a6452850cc8f356e50d43948ebed05b39f2ac498
PNZBEIJINGL <63351525@qq.com>
2019-08-18 22:12:29 +0800

    1011:wait(long)可以被其他线程唤醒

--- src/main/java/com/programing/communication/WaitTest.java
+++ src/main/java/com/programing/communication/WaitTest.java
@@ -19,9 +19,24 @@ public class WaitTest {
         }
     };
 
-    public static void main(String[] args) {
-        Thread thread=new Thread(runable);
+    static private Runnable runable2 = new Runnable() {
+        public void run() {
+            synchronized (lock) {
+                System.out.println(Thread.currentThread().getName() + "|" + System.currentTimeMillis());
+                lock.notify();
+                System.out.println(Thread.currentThread().getName() + "|" + System.currentTimeMillis());
+            }
+        }
+    };
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(runable);
         thread.start();
+
+        Thread.sleep(3000);
+        Thread thread2 = new Thread(runable2);
+        thread2.start();
     }
 
 }
