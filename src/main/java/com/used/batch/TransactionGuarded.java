c5cafcedad02e69bc5ea3181f70876ff1d574571
liuyong <liuyong@startimes.com.cn>
2019-08-20 00:22:03 +0800

    batch process

--- src/main/java/com/used/batch/TransactionGuarded.java
+++ src/main/java/com/used/batch/TransactionGuarded.java
@@ -1,14 +1,9 @@
 /***********************************************************************
- *
- * [�ļ�������Ϣ]
- *
- * stariboss-accept 1.0Դ���뿽��Ȩ�������Ĵ�ʱ������������޹�˾���У�
- * �ܵ����ɵı������κι�˾����ˣ�δ����Ȩ�������Կ�����    
- *
+
  * @copyright Copyright: 2002-2015 Beijing Startimes
  * Software Technology Co. Ltd.
  * @creator 10001874 10001874@startimes.com.cn
- * @create-time 2018-5-31 ����11:52:06
+ * @create-time 2018-5-3111:52:06
  * @revision $Id: TransactionGuarded.java,v 1.1.2.6 2018/11/26 07:08:55 liuyong Exp $
  *
  ***********************************************************************/
@@ -16,15 +11,12 @@ package com.used.batch;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 /**
  * @author LIUY ���������(�����ػ���)
  */
 public class TransactionGuarded {
 
-	private final Log logger = LogFactory.getLog(getClass());
+	//private final Log logger = LogFactory.getLog(getClass());
 
 	private final int number; // �ػ����߳�����
 	private AtomicInteger currentNum = new AtomicInteger(0);
@@ -52,19 +44,19 @@ public class TransactionGuarded {
 
 		int num = currentNum.incrementAndGet();
 		if (num == number) {
-			logger.info(threadName + " all successful, do weak up");
+			//logger.info(threadName + " all successful, do weak up");
 			status = State.SUCESSES;
 			weakUpAll();
 		} else {
-			logger.info(threadName + " is waiting, " + num + "/" + number);
+			//logger.info(threadName + " is waiting, " + num + "/" + number);
 			doHold();
 		}
 	}
 
 	public synchronized void setFailed() {
 		if (!status.equals(State.FAILED)) {
-			logger.info(Thread.currentThread().getName()
-					+ " is error, do weak up");
+			//logger.info(Thread.currentThread().getName()
+			//		+ " is error, do weak up");
 			status = State.FAILED;
 			weakUpAll();
 		}
@@ -74,18 +66,15 @@ public class TransactionGuarded {
 		try {
 			this.wait(45 * 60 * 1000);
 			if (status.equals(State.WORKING)) {
-				logger.error(Thread.currentThread().getName()+ " weak up for holding time out");
+				//logger.error(Thread.currentThread().getName()+ " weak up for holding time out");
 				setFailed();
 			}
 		} catch (Exception e) {
-			logger.error(Thread.currentThread().getName()+" Holding error:", e);
+			//logger.error(Thread.currentThread().getName()+" Holding error:", e);
 			setFailed();
 		}
 	}
 
-	/**
-	 * ���������߳�
-	 */
 	private void weakUpAll() {
 		this.notifyAll();
 	}
--- src/main/java/com/used/specification/MainTest.java
+++ src/main/java/com/used/specification/MainTest.java
@@ -1,8 +1,5 @@
 package com.used.specification;
 
-import com.sun.tools.javac.jvm.Code;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
