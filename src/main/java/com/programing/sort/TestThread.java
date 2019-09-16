b2a16e0a6e60984708b74c267693ad5fe6d55fcd
liuyong <liuyong@startimes.com.cn>
2019-09-16 10:12:20 +0800

    0753 使线程具有有序性

---
+++ src/main/java/com/programing/sort/TestThread.java
@@ -0,0 +1,50 @@
+package com.programing.sort;
+
+public class TestThread extends Thread {
+
+    private Object lock;
+    private String showMsg;
+    private int position;  //位置顺序
+    private int printCount = 0;  //打印
+    volatile private static int addNumber = 1;
+
+    public TestThread(Object lock, String showMsg, int position) {
+        super();
+        this.lock = lock;
+        this.showMsg = showMsg;
+        this.position = position;
+    }
+
+    @Override
+    public void run() {
+        try {
+            synchronized (lock) {
+                while (true) {
+                    if (addNumber % 3 == position) {
+                        System.out.println(Thread.currentThread().getName() + " runCount=" + addNumber + " " + showMsg);
+                        lock.notifyAll();
+                        addNumber++;
+                        printCount++;
+                        if (printCount == 1) {  //循环次数
+                            break;
+                        }
+                    } else {
+                        lock.wait();
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Object lock = new Object();
+        TestThread a = new TestThread(lock, "DO A", 1);
+        TestThread b = new TestThread(lock, "DO B", 2);
+        TestThread c = new TestThread(lock, "DO C", 0);
+        a.start();
+        b.start();
+        c.start();
+    }
+}
